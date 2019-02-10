aaefd10fc38feb20cb62c71d65c39e0a3fb7b8ec
narendra67 <mvnarendrareddy@gmail.com>
2019-02-11 00:12:08 +0530

    Updates

---
+++ precache-manifest.350ac586b6939170c810627a0bb84d82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed14b5ed069efcfac693",
+    "url": "/movie-analysis/static/js/runtime~main.ed14b5ed.js"
+  },
+  {
+    "revision": "e88bc61172d94a62c348",
+    "url": "/movie-analysis/static/js/main.e88bc611.chunk.js"
+  },
+  {
+    "revision": "950a3e44cb17bc96e377",
+    "url": "/movie-analysis/static/js/1.950a3e44.chunk.js"
+  },
+  {
+    "revision": "e88bc61172d94a62c348",
+    "url": "/movie-analysis/static/css/main.54f6071a.chunk.css"
+  },
+  {
+    "revision": "19b649d5cc409a1e692a2a46d37c727c",
+    "url": "/movie-analysis/index.html"
+  }
+];
